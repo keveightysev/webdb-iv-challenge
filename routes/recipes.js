c4a0eff6fff5befaeb1ce2da6ba5dbaeaa96e3f1
keveightysev <keveightysev@gmail.com>
2019-03-28 17:29:40 -0500

    Recipet POST working

--- routes/recipes.js
+++ routes/recipes.js
@@ -12,4 +12,22 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.post('/', async (req, res) => {
+  if (!req.body.name) {
+    res.status(406).json({ message: 'Please enter a name for the recipe' });
+    return;
+  } else if (!req.body.dish_id) {
+    res
+      .status(406)
+      .json({ message: 'Please include a dish ID for the recipe' });
+    return;
+  }
+  try {
+    const recipe = await Recipes.addRecipe(req.body);
+    res.status(201).json(recipe);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
